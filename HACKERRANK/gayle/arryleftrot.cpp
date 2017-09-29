32912daa31171f8cd10405be4b0ae8aece2feffb
GAUTHAM M <gauthamm2015@gmail.com>
2017-09-29 21:34:39 +0530

    back up losst

---
+++ HACKERRANK/gayle/arryleftrot.cpp
@@ -0,0 +1,61 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> array_left_rotation(vector<int> a, int n, int k) {
+    vector<int>temp(k);
+    for(int i=0;i<k;++i)
+    {
+        temp[i]=a[i];
+    }
+    for(int i=k;i<n;++i)
+    {
+        a[i-k]=a[i];
+    }
+    int ctr=0;
+    for(int i=n-k;i<n;++i)
+    {
+        a[i]=temp[ctr];
+        ++ctr;
+            
+        
+    }
+    return a;
+    
+}
+
+int main(){
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int a_i = 0;a_i < n;a_i++){
+        cin >> a[a_i];
+    }
+    vector<int> output = array_left_rotation(a, n, k);
+    for(int i = 0; i < n;i++)
+        cout << output[i] << " ";
+    cout << endl;
+    return 0;
+}
