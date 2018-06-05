509757b388ba51304fb19f9df512f5e34deecbd2
GauthamMaraswami <15co118.gautham@nitk.edu.in>
2018-06-05 09:59:59 +0530

    practice

---
+++ Interviewbits/Math/rearrange_array.cpp
@@ -0,0 +1,15 @@
+
+
+    void Solution::arrange(vector<int> &A) {
+        int len=A.size();
+        for(int i=0;i<len;++i)
+        {
+            A[i]=A[i]+(A[A[i]]%len)*len;
+        }
+        for(int i=0;i<len;++i)
+        {
+            A[i]=A[i]/len;
+        }
+    }
+
+
